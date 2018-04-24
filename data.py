f3655f68fa050a3fa248163aaae9e7eae04bda91
Richard Strub <rstrub@gs6102dsc-invenio.gesdisc.eosdis.nasa.gov>
2018-04-24 18:54:55 +0000

    arranged data.py somewhat

--- data.py
+++ data.py
@@ -16,11 +16,18 @@ def createLabel(filepath):
     label = label.replace(m.group(0),"QCSUB" + m.group(0).upper())
     return label 
 
-# If included then these are included List:
-# So we also need to determine which main variables are in which section:
-# trying to find where I classify the main variables ...
+# Which variables are available for screening and which group they belong to. This was retrieved from useperl.pl
+hash = {}
+hash["CO_Variables"] = {"CO_total_column":1,"COVMRLevStd":1}
+hash["Cloud_Variables"] = ("CldFrcStd","CldFrcTot","PCldTop","TCldTop",)
+hash["Radiation_Variables"] = ("olr3x3","olr","clrolr",)
+hash["Methane_Variables"] = ("CH4VMRLevStd","CH4_total_column",)
+hash["Ozone_Variables"] = ("O3VMRLevStd","O3VMRStd","totO3Std",)
+hash["Full_Swath_Data_Fields_Variables"] = ("GP_Surface","EmisMWStd","GP_Height","GP_Tropopause","GP_Height_MWOnly","sfcTbMWStd","emisIRStd",)
+hash["Temperature_Variables"] = ("TSurfStd","TAirStd","TAirMWOnlyStd","TSurfAir",)
+hash["Moisture_Variables"] = ("H2OMMRStd","H2OMMRLevStd","H2OMMRSat","RelHumSurf","totCldH2OStd","totH2OMWOnlyStd","totH2OStd","RelHum","RelHumSurf_liquid","RelHum_liquid","H2OMMRSatSurf_liquid","H2OMMRSatLevStd_liquid","H2OMMRSatSurf","H2OMMRSurf","H2OMMRSat_liquid","H2OMMRSatLevStd")
 
-# Which variables need to be included in the subset. This was retrieved from the ServiceConfig.pm
+# SUPPORTING DATA: Leave these alone: 
 support = {}
 support["Moisture_Variables"]  = ('H2OMMRLevStd_QC', 'H2OMMRSat_QC', 'H2OMMRSat_liquid_QC', 'H2OMMRStd_QC', 'totCldH2OStd_QC',
            'totH2OMWOnlyStd_QC', 'totH2OStd_QC', 'RelHumSurf_QC', 'RelHum_QC', 'RelHumSurf_liquid_QC', 
@@ -34,19 +41,14 @@ Methane   = ['CH4VMRLevStd_QC', 'CH4_total_column_QC' ],
 Full_Swath_Data_Fields = ['EmisMWStd_QC', 'sfcTbMWStd_QC', 'emisIRStd_QC', 'GP_Height_QC', 'GP_Height_MWOnly_QC', 'GP_Surface_QC', 'GP_Tropopause_QC' ],
 Ozone = [ 'O3VMRStd_QC', 'totO3Std_QC', 'O3VMRLevStd_QC']
 
-# Which variables are available for screening and which group they belong to. This was retrieved from useperl.pl
-hash = {}
-hash["CO_Variables"] = {"CO_total_column":1,"COVMRLevStd":1}
-hash["Cloud_Variables"] = ("CldFrcStd","CldFrcTot","PCldTop","TCldTop",)
-hash["Radiation_Variables"] = ("olr3x3","olr","clrolr",)
-hash["Methane_Variables"] = ("CH4VMRLevStd","CH4_total_column",)
-hash["Ozone_Variables"] = ("O3VMRLevStd","O3VMRStd","totO3Std",)
-hash["Full_Swath_Data_Fields_Variables"] = ("GP_Surface","EmisMWStd","GP_Height","GP_Tropopause","GP_Height_MWOnly","sfcTbMWStd","emisIRStd",)
-hash["Temperature_Variables"] = ("TSurfStd","TAirStd","TAirMWOnlyStd","TSurfAir",)
-hash["Moisture_Variables"] = ("H2OMMRStd","H2OMMRLevStd","H2OMMRSat","RelHumSurf","totCldH2OStd","totH2OMWOnlyStd","totH2OStd","RelHum","RelHumSurf_liquid","RelHum_liquid","H2OMMRSatSurf_liquid","H2OMMRSatLevStd_liquid","H2OMMRSatSurf","H2OMMRSurf","H2OMMRSat_liquid","H2OMMRSatLevStd")
-
+# SCREENABLE DATA BACKUP LIST
 screenable = \
  ("CO_total_column","COVMRLevStd", "CldFrcStd","CldFrcTot","PCldTop","TCldTop", "olr3x3","olr","clrolr", "CH4VMRLevStd","CH4_total_column", "O3VMRLevStd","O3VMRStd","totO3Std", "GP_Surface","EmisMWStd","GP_Height","GP_Tropopause","GP_Height_MWOnly","sfcTbMWStd","emisIRStd", "TSurfStd","TAirStd","TAirMWOnlyStd","TSurfAir", "H2OMMRStd","H2OMMRLevStd","H2OMMRSat","RelHumSurf","totCldH2OStd","totH2OMWOnlyStd","totH2OStd","RelHum","RelHumSurf_liquid","RelHum_liquid","H2OMMRSatSurf_liquid","H2OMMRSatLevStd_liquid","H2OMMRSatSurf","H2OMMRSurf","H2OMMRSat_liquid","H2OMMRSatLevStd")
+
+# ANCILLARY DATA BACKUP LIST
+ancillary = ('AMSU_Chans_Resid', 'CC1_Resid', 'CC1_noise_eff_amp_factor', 'CC_noise_eff_amp_factor', 'CCfinal_Noise_Amp', 'CCfinal_Resid', 'Cloud_Resid_Ratio', 'EmisMWStd', 'EmisMWStdErr', 'GP_Height', 'GP_Height_MWOnly', 'GP_Surface', 'GP_Tropopause', 'Initial_CC_score', 'MW_ret_used', 'PTropopause', 'Qual_Guess_PSurf', 'RetQAFlag', 'Startup', 'Surf_Resid_Ratio', 'TSurfdiff_IR_4CC1', 'TSurfdiff_IR_4CC2', 'T_Tropopause', 'Tdiff_IR_4CC1', 'Tdiff_IR_MW_ret', 'Temp_Resid_Ratio', 'TotCld_4_CCfinal', 'Water_Resid_Ratio', 'all_spots_avg', 'demgeoqa', 'dust_flag', 'ftptgeoqa', 'glintgeoqa', 'glintlat', 'glintlon', 'landFrac', 'landFrac_err', 'moongeoqa', 'nadirTAI', 'numHingeSurf', 'num_clear_spectral_indicator', 'retrieval_type', 'sat_lat', 'sat_lon', 'satazi', 'satgeoqa', 'satheight', 'satpitch', 'satroll', 'satyaw', 'satzen', 'scan_node_type', 'sfcTbMWStd', 'solazi', 'solzen', 'spectral_clear_indicator', 'sun_glint_distance', 'topog', 'topog_err', 'zengeoqa')
+
+
 
 best=(
 "COVMRLevStd",
@@ -98,54 +100,7 @@ screenable_edit = (
 "H2OMMRSat_liquid", 
 "H2OMMRSatLevStd")
 
-screenable_backup = (
-"CO_total_column",
-"COVMRLevStd",
-"CldFrcStd",
-"CldFrcTot",
-"PCldTop",
-"TCldTop",
-"olr3x3",
-"olr",
-"clrolr",
-"CH4VMRLevStd",
-"CH4_total_column",
-"O3VMRLevStd",
-"O3VMRStd",
-"totO3Std",
-"GP_Surface",
-"EmisMWStd",
-"GP_Height",
-"GP_Tropopause",
-"GP_Height_MWOnly",
-"sfcTbMWStd",
-"emisIRStd",
-"TSurfStd",
-"TAirStd",
-"TAirMWOnlyStd",
-"TSurfAir",
-"H2OMMRStd",
-"H2OMMRLevStd",
-"H2OMMRSat",
-"RelHumSurf",
-"totCldH2OStd",
-"totH2OMWOnlyStd",
-"totH2OStd",
-"RelHum",
-"RelHumSurf_liquid",
-"RelHum_liquid",
-"H2OMMRSatSurf_liquid",
-"H2OMMRSatLevStd_liquid",
-"H2OMMRSatSurf",
-"H2OMMRSurf",
-"H2OMMRSat_liquid", 
-"H2OMMRSatLevStd")
 
-# for ancillary have 2 lists:
-# 1 main list
-# 1 editable list
-# 1 backup list
-ancillary = ('AMSU_Chans_Resid', 'CC1_Resid', 'CC1_noise_eff_amp_factor', 'CC_noise_eff_amp_factor', 'CCfinal_Noise_Amp', 'CCfinal_Resid', 'Cloud_Resid_Ratio', 'EmisMWStd', 'EmisMWStdErr', 'GP_Height', 'GP_Height_MWOnly', 'GP_Surface', 'GP_Tropopause', 'Initial_CC_score', 'MW_ret_used', 'PTropopause', 'Qual_Guess_PSurf', 'RetQAFlag', 'Startup', 'Surf_Resid_Ratio', 'TSurfdiff_IR_4CC1', 'TSurfdiff_IR_4CC2', 'T_Tropopause', 'Tdiff_IR_4CC1', 'Tdiff_IR_MW_ret', 'Temp_Resid_Ratio', 'TotCld_4_CCfinal', 'Water_Resid_Ratio', 'all_spots_avg', 'demgeoqa', 'dust_flag', 'ftptgeoqa', 'glintgeoqa', 'glintlat', 'glintlon', 'landFrac', 'landFrac_err', 'moongeoqa', 'nadirTAI', 'numHingeSurf', 'num_clear_spectral_indicator', 'retrieval_type', 'sat_lat', 'sat_lon', 'satazi', 'satgeoqa', 'satheight', 'satpitch', 'satroll', 'satyaw', 'satzen', 'scan_node_type', 'sfcTbMWStd', 'solazi', 'solzen', 'spectral_clear_indicator', 'sun_glint_distance', 'topog', 'topog_err', 'zengeoqa')
 
 ancillary_include = (
 'AMSU_Chans_Resid', 
@@ -222,67 +177,6 @@ ancillary_donotinclude = (
 'topog_err', 
 'zengeoqa')
 
-ancillary_backup = (
-'AMSU_Chans_Resid', 
-'CC1_Resid', 
-'CC1_noise_eff_amp_factor', 
-'CC_noise_eff_amp_factor', 
-'CCfinal_Noise_Amp', 
-'CCfinal_Resid', 
-'Cloud_Resid_Ratio', 
-'EmisMWStd', 
-'EmisMWStdErr', 
-'GP_Height', 
-'GP_Height_MWOnly', 
-'GP_Surface', 
-'GP_Tropopause', 
-'Initial_CC_score', 
-'MW_ret_used', 
-'PTropopause', 
-'Qual_Guess_PSurf', 
-'RetQAFlag', 
-'Startup', 
-'Surf_Resid_Ratio', 
-'TSurfdiff_IR_4CC1', 
-'TSurfdiff_IR_4CC2', 
-'T_Tropopause', 
-'Tdiff_IR_4CC1', 
-'Tdiff_IR_MW_ret', 
-'Temp_Resid_Ratio', 
-'TotCld_4_CCfinal', 
-'Water_Resid_Ratio', 
-'all_spots_avg', 
-'demgeoqa', 
-'dust_flag', 
-'ftptgeoqa', 
-'glintgeoqa', 
-'glintlat', 
-'glintlon', 
-'landFrac', 
-'landFrac_err', 
-'moongeoqa', 
-'nadirTAI', 
-'numHingeSurf', 
-'num_clear_spectral_indicator', 
-'retrieval_type', 
-'sat_lat', 
-'sat_lon', 
-'satazi', 
-'satgeoqa', 
-'satheight', 
-'satpitch', 
-'satroll', 
-'satyaw', 
-'satzen', 
-'scan_node_type', 
-'sfcTbMWStd', 
-'solazi', 
-'solzen', 
-'spectral_clear_indicator', 
-'sun_glint_distance', 
-'topog', 
-'topog_err', 
-'zengeoqa')
 
 # So algorithm is to:
 # enable any ancillary
